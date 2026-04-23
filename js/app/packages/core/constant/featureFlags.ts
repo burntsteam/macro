9a447414dfd2181a3ea810304360dd88e6d789ad
dev-rb <43100342+dev-rb@users.noreply.github.com>
2026-04-23 17:41:24 -0400

    feat(teams): onboarding step for creating team and inviting members (#2811)

--- js/app/packages/core/constant/featureFlags.ts
+++ js/app/packages/core/constant/featureFlags.ts
@@ -329,3 +329,7 @@ export function ENABLE_CALLS(): boolean {
 
   return analytics.posthog.isFeatureEnabled('enable-calls') ?? false;
 }
+
+export const ENABLE_INVITE_TEAM_ONBOARDING_OVERRIDE = DEV_MODE_ENV
+  ? true
+  : undefined;
